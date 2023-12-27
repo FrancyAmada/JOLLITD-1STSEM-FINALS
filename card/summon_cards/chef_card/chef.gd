0dc53de15a35904affa88710ebf496ff9467ff1d
code1426 <gmver27@gmail.com>
2023-12-27 12:07:08 +0800

    Added: Chef (unfinished)

---
+++ card/summon_cards/chef_card/chef.gd
@@ -0,0 +1,68 @@
+extends Summon
+
+class_name chef
+
+@onready var attack_component: AttackComponent = $AttackComponent
+@onready var particle: GPUParticles2D = $GPUParticles2D
+
+@onready var target = null
+
+func _ready():
+	set_healthbar()
+	set_direction()
+	animation_component.connect("animation_is_finished", _on_animation_finished)
+	particle.emitting = false
+
+func _physics_process(delta):
+	effects_component.process(delta)
+#	print(" Value:", get_node("Sprite2D").modulate, "")
+	attack_component.process(delta)
+	target_detection.process(delta)
+#	print(name, " ID:", summon_id, " Target:", target)
+	set_target()
+	set_direction()
+	animation_component.update_facing_direction(direction)
+
+	if !on_attack_range and direction != Vector2.ZERO:
+		velocity = direction * speed
+		
+	# Character has stopped and there is an enemy
+	elif on_attack_range and target != null:
+		attack_component.use_attack(target)
+		direction = Vector2.ZERO
+		velocity = Vector2.ZERO
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.y = move_toward(velocity.y, 0, speed)
+
+	move_and_slide()
+	animation_component.update_animation(direction)
+	update_healthbar()
+
+func set_target():
+	target = target_detection.get_target()
+#	if target != null:
+#		print_debug(target, " direction: ", direction)
+
+func go_to_enemy_base():
+	if summon_id == 1:
+		direction = Vector2.RIGHT
+	else:
+		direction = Vector2.LEFT
+
+func go_to_target():
+	direction = (target.global_position - global_position).normalized()
+	var distance: float = global_position.distance_to(target.global_position)
+	if distance <= attack_range:
+		on_attack_range = true
+
+func set_direction():
+	on_attack_range = false
+	if target == null:
+		go_to_enemy_base()
+	else:
+		go_to_target()
+
+func _on_animation_finished(anim_name: String):
+	if anim_name == "Death":
+		queue_free()
---
+++ card/summon_cards/chef_card/chef_card.gd
@@ -0,0 +1,23 @@
+extends Card
+
+var chef: PackedScene = preload("res://card/summon_cards/chef_card/chef.tscn")
+
+
+func place(place_position: Vector2):
+	if is_grabbed and in_valid_area and in_placement_area:
+		get_parent().get_parent().player.placed_card = true
+		print_debug("Placed at: ", place_position)
+		summon(place_position)
+		queue_free()
+	else:
+		un_grab()
+		return_to_position()
+		
+func summon(place_position: Vector2):
+	for i in range(2):
+		var new_chef = chef.instantiate()
+		summon_node.add_child(new_chef)
+		new_chef.set_summon_id(player_id)
+#		print_debug("Player ID: ", player_id, " Summon:", new_waiter)
+		new_chef.global_position = place_position + Vector2(randi_range(-50, 50), randi_range(-50, 50))
+		
